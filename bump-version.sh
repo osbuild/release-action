10505250387bb448a6a54edfc07ba5a8f066f290
Simon Steinbeiss <simon.steinbeiss@redhat.com>
2022-06-28 15:57:45 +0200

    Bump version in osbuild/__init__.py as well
    This change is so that the `osbuild --version` command works as it
    should.
    
    See https://github.com/osbuild/osbuild/pull/1039

--- bump-version.sh
+++ bump-version.sh
@@ -7,3 +7,7 @@ sed -i -E "s/(Version:\\s+)[0-9]+/\1$VERSION/" *osbuild*.spec
 if [ -f "setup.py" ]; then
   sed -i -E "s/(version=\")[0-9]+/\1$VERSION/" setup.py
 fi
+
+if [ -f "osbuild/__init__.py" ]; then
+  sed -i "s|__version__ = \"$(VERSION)\"|__version__ = \"$(NEXT_VERSION)\"|" osbuild/__init__.py
+fi
