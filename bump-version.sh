15b44933593b5902efc8f5e6508526d9cfc5ac8f
Florian Schüller <florian.schueller@redhat.com>
2024-12-20 14:29:20 +0100

    bump-version.sh: support image-builder* projects

--- bump-version.sh
+++ bump-version.sh
@@ -2,7 +2,13 @@
 
 VERSION=$(( $1 + 1 ))
 
-sed -i -E "s/(Version:\\s+)[0-9]+/\1$VERSION/" *osbuild*.spec
+# Enable nullglob to avoid errors on unmatched patterns
+# we expect _either_ *osbuild* or *image-builder*
+shopt -s nullglob
+sed -i -E "s/(Version:\\s+)[0-9]+/\1$VERSION/" *osbuild*.spec *image-builder*.spec
+
+# Disable nullglob to restore default behavior
+shopt -u nullglob
 
 if [ -f "setup.py" ]; then
   sed -i -E "s/(version=\")[0-9]+/\1$VERSION/" setup.py
