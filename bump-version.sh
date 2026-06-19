bd1160e3466c12bd3d168af670f4bcb162c74bad
Simon de Vlieger <cmdr@supakeen.com>
2026-06-19 09:35:55 +0200

    many: support semver/bump type
    Support the same arguments as for the create-tag branch for the
    bump-version script so it can be configured to bump major/minor/patch.
    Default behavior stays the same.
    
    Signed-off-by: Simon de Vlieger <cmdr@supakeen.com>

--- bump-version.sh
+++ bump-version.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <version>"
+  echo "Usage: $0 <version> [major|minor|patch]"
   exit 1
 fi
 
-if [[ "$1" =~ \. ]]; then
-  # If there is a "dot" in the version number, we need to bump the last number
-  VERSION=$(echo "$1" | sed -E 's/(.*\.)([0-9]+)$/echo "\1$((\2+1))"/e')
+CURRENT="$1"
+BUMP_TYPE="${2:-}"
+
+if [ -n "$BUMP_TYPE" ]; then
+  IFS='.' read -r MAJOR MINOR PATCH <<< "$CURRENT"
+  MINOR="${MINOR:-0}"
+  PATCH="${PATCH:-0}"
+
+  case "$BUMP_TYPE" in
+    major)
+      VERSION="$((MAJOR + 1)).0.0"
+      ;;
+    minor)
+      VERSION="${MAJOR}.$((MINOR + 1)).0"
+      ;;
+    patch)
+      VERSION="${MAJOR}.${MINOR}.$((PATCH + 1))"
+      ;;
+    *)
+      echo "Error: bump type must be 'major', 'minor', or 'patch'"
+      exit 1
+      ;;
+  esac
+elif [[ "$CURRENT" =~ \. ]]; then
+  VERSION=$(echo "$CURRENT" | sed -E 's/(.*\.)([0-9]+)$/echo "\1$((\2+1))"/e')
 else
-  # If there is no "dot" in the version number, we simply increment the version number
-  VERSION=$(( $1 + 1 ))
+  VERSION=$(( CURRENT + 1 ))
 fi
 
 
 find -name "*osbuild*.spec" -or -name "cockpit-*.spec" -or -name "image-builder*.spec" \
-    | xargs sed -i -E "s/(Version:\\s+)[0-9]+/\1$VERSION/"
+    | xargs sed -i -E "s/(Version:\\s+)[0-9]+[0-9.]*/\1$VERSION/"
 
 if [ -f "setup.py" ]; then
-  sed -i -E "s/(version=\")[0-9]+/\1$VERSION/" setup.py
+  sed -i -E "s/(version=\")[0-9]+[0-9.]*/\1$VERSION/" setup.py
 fi
 
 if [ -f "osbuild/__init__.py" ]; then
-  sed -i -E "s/(__version__ = \")[0-9]+/\1$VERSION/" osbuild/__init__.py
+  sed -i -E "s/(__version__ = \")[0-9]+[0-9.]*/\1$VERSION/" osbuild/__init__.py
 fi
